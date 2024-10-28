7bc2b7fd40f7adbbbc64b3a18ae611d94d81fcef
geandrodatzuk <geandro.datzuk@escola.pr.gov.br>
2024-10-28 12:48:35 +0000

    28/10

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal=document.querySelector("caixa__principal");
+const caixaPrincipal=document.querySelector("caixa__perguntas");
+const caixaAlternativa=document.querySelector(".caixa__alternativa");
+const caixaAlternativa=document.querySelector(".caixa__resultados");
+const caixatextoresulado=document.querySelector(".texto__resultado");
